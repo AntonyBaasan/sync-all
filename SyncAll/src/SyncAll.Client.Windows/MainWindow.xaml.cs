fbd755958b7b446a68797b6ea2f13e2e728238e4
Antony Baasandorj <abaasandorj@microsoft.com>
2024-04-26 00:37:36 -0400

    add components

--- SyncAll/src/SyncAll.Client.Windows/MainWindow.xaml.cs
+++ SyncAll/src/SyncAll.Client.Windows/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace SyncAll.Client.Windows
 {
@@ -10,6 +11,30 @@ namespace SyncAll.Client.Windows
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Bold_Checked(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Bold_Unchecked(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Italic_Checked(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Italic_Unchecked(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void IncreaseFont_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void DecreaseFont_Click(object sender, RoutedEventArgs e)
+        {
         }
     }
 }
